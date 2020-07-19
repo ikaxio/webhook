0292af0a35f6d25649e2d05f08d66a0794a9827f
karsure <591524605.com>
2020-07-19 15:48:09 +0800

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ http
   .createServer(function (req, res) {
     handler(req, res, function (err) {
       res.statusCode = 404
-      res.end("happy to code aaa")
+      res.end("happy to code bbb")
     })
   })
   .listen(14110)

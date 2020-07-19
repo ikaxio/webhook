e48e58b9b71f6bf6d96e4e4c43ff3682bdac3969
karsure <591524605.com>
2020-07-19 18:15:15 +0800

    a

--- utils/shell/deploy_dev.sh
+++ utils/shell/deploy_dev.sh
@@ -4,8 +4,11 @@ WEB_ROOT='/data/workspace/webhook'
 echo 'start'
 cd $WEB_ROOT
 echo "pulling source code"
+git reset --hard origin/master
+git clean -f
 git pull
+git checkout .
 echo "pull finished"
 echo "npm ci"
-npm i
+npm ci
 echo "end"
